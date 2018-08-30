e60ea60da1cf1839e4a7b721fb019556af9b0c28
ty <towyuan@outlook.com>
2018-08-30 18:40:09 +0800

    Refactor

--- src/defines.ts
+++ src/defines.ts
@@ -1,14 +1,6 @@
-export interface Reader {
-  (i: number, dest: Buffer): Promise<Buffer>;
-}
-
-export interface Writer {
-  (i: number, data: Buffer): Promise<{ bytesWrite: number, buffer: Buffer }>;
-}
-
 export interface Driver {
   sectorSize: number;
   numSectors: number;
-  readSectors: (i: number, dest: Buffer) => Promise<Buffer>;
-  writeSectors: (i: number, data: Buffer) => Promise<number>;
+  readSectors(i: number, dest: Buffer): Promise<Buffer>;
+  writeSectors(i: number, data: Buffer): Promise<number>;
 }
